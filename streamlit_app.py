0105ff5d255fe64e96b3ac30d40a957d6e04d041
amitjivancerts <129454334+amitjivancerts@users.noreply.github.com>
2023-04-03 16:04:05 +0200

    Update streamlit_app.py
    API request

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
 streamlit.dataframe(fruits_to_show)
